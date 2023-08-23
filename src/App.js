64f537364f2575ce97cbd78307a2714ab018b711
Rixon <rixonraj16@gmail.com>
2023-08-24 03:11:10 +0530

    fp1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Routes, Route, useParams, Link, Navigate } from 'react-router-dom'
+import Login from './Components/Login';
+import Register from './Components/Register';
+import Home from './Components/Home';
+import Forgotpassword from './Components/Forgotpassword';
+import Newpassword from './Components/Newpassword';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+        <Route path='/reset' element={<Forgotpassword />}></Route>
+        <Route path='/updatePassword/:id' element={<Newpassword />}></Route>
+        <Route path='/home'>
+          <Route path=':userid' element={<Home />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
